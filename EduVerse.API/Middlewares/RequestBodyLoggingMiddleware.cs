00ad8c8a8ce813acae832ba764051061950349aa
Kashyap <kashyapchudasama0@gmail.com>
2025-04-03 19:47:23 +0530

    SeriLog & Application Insights added

---
+++ EduVerse.API/Middlewares/RequestBodyLoggingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.ApplicationInsights.DataContracts;
+using Serilog;
+using System.Text;
+
+namespace EduVerse.API.Middlewares
+{
+    public class RequestBodyLoggingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var method = context.Request.Method;
+
+            //Ensure request body can be read multiple times
+            context.Request.EnableBuffering();
+
+            //only, if we're dealing with POST,PUT or GET
+            if (context.Request.Body.CanRead && (method == HttpMethods.Post || method == HttpMethods.Put))
+            {
+                // Leave stream open so next middleware can read it
+                using var reader = new StreamReader(
+                    context.Request.Body,
+                    Encoding.UTF8,
+                    detectEncodingFromByteOrderMarks: false,
+                    bufferSize: 512, leaveOpen: true);
+                var requestBody = await reader.ReadToEndAsync();
+                // Reset stream position, so next middleware can read it
+                context.Request.Body.Position = 0;
+                // Write request body to App Insights
+                var requestTelemetry = context.Features.Get<RequestTelemetry>();
+                requestTelemetry?.Properties.Add("RequestBody", requestBody);
+                Log.Information("Request:" + requestBody);
+            }
+            // Call next middleware in the pipeline
+            await next(context);
+        }
+    }
+}
--- EduVerse.API/Program.cs
+++ EduVerse.API/Program.cs
@@ -1,10 +1,16 @@
 
+using EduVerse.API.Middlewares;
 using EduVerse.Data;
 using EduVerse.Data.Contract;
 using EduVerse.Data.Implementation;
 using EduVerse.Service.Contract;
 using EduVerse.Service.Implementation;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Templates;
+using System.Net;
 
 namespace EduVerse.API
 {
@@ -12,68 +18,129 @@ namespace EduVerse.API
     {
         public static void Main(string[] args)
         {
-            #region Service Configuration
-            var builder = WebApplication.CreateBuilder(args);
-           
-            //Database Configuration
-            var configuration = builder.Configuration;
-            builder.Services.AddDbContextPool<EduVerseDbContext>(options =>
+            // Configure Serilog with the settings
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()
+            .CreateBootstrapLogger();
+
+            try
             {
-                options.UseSqlServer(
-                    configuration.GetConnectionString("EduVerseDbContext"),
-                    providerOptions => providerOptions.EnableRetryOnFailure());
-                //Only for development purpose not go to PRODUCTION
-                //options.EnableServiceProviderCaching();
-            });
-            //Cors for connection server routes to client app
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("default", policy =>
+                #region Service Configuration
+                var builder = WebApplication.CreateBuilder(args);
+
+                //Seri log & API-insights
+                builder.Services.AddApplicationInsightsTelemetry();
+
+                builder.Host.UseSerilog((context, services, loggerConfiguration) => loggerConfiguration
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .WriteTo.Console(new ExpressionTemplate(
+                    // Include trace and span ids when present.
+                    "[{@t:HH:mm:ss} {@l:u3}{#if @tr is not null} ({substring(@tr,0,4)}:{substring(@sp,0,4)}){#end}] {@m}\n{@x}"))
+                .WriteTo.ApplicationInsights(
+                  services.GetRequiredService<TelemetryConfiguration>(),
+                  TelemetryConverter.Traces));
+
+                Log.Information("Starting the EduVerseByKashyap API...");
+
+                //Database Configuration
+                var configuration = builder.Configuration;
+                builder.Services.AddDbContextPool<EduVerseDbContext>(options =>
                 {
-                    policy.WithOrigins("http://localhost:4200") // Corrected frontend URL without trailing slash
-                          .AllowAnyHeader()
-                          .AllowAnyMethod()
-                          .AllowCredentials();  // Required for SignalR
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("EduVerseDbContext"),
+                        providerOptions => providerOptions.EnableRetryOnFailure());
+                    //Only for development purpose not go to PRODUCTION
+                    //options.EnableServiceProviderCaching();
                 });
-            });
-            // Add services to the container.
+                //Cors for connection server routes to client app
+                builder.Services.AddCors(options =>
+                {
+                    options.AddPolicy("default", policy =>
+                    {
+                        policy.WithOrigins("http://localhost:4200") // Corrected frontend URL without trailing slash
+                              .AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowCredentials();  // Required for SignalR
+                    });
+                });
+                // Add services to the container.
 
-            builder.Services.AddControllers();
-            
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+                builder.Services.AddControllers();
 
-            //configure services DI here
-            //AddScoped : when a request is hit till the request is completely processed and return only one instance of requested class will be given
-            //AddTransient : when a request is hit till the request is completely processed and return new instance of requested class will be given
-            //Addsingletone : only one time for the whole application
-            builder.Services.AddScoped<ICourseCategoryRepository, CourseCategoryRepository>();
-            builder.Services.AddScoped<ICourseCategoryService, CourseCategoryService>();
-            builder.Services.AddScoped<ICourseRepository, CourseRepository>();
-            builder.Services.AddScoped<ICourseService, CourseService>();
-            #endregion
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
 
-            #region Middlewares
-            var app = builder.Build();
+                //configure services DI here
+                //AddScoped : when a request is hit till the request is completely processed and return only one instance of requested class will be given
+                //AddTransient : when a request is hit till the request is completely processed and return new instance of requested class will be given
+                //Addsingletone : only one time for the whole application
+                builder.Services.AddScoped<ICourseCategoryRepository, CourseCategoryRepository>();
+                builder.Services.AddScoped<ICourseCategoryService, CourseCategoryService>();
+                builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+                builder.Services.AddScoped<ICourseService, CourseService>();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                //for seri log Application insights
+                builder.Services.AddTransient<RequestBodyLoggingMiddleware>();
+                builder.Services.AddTransient<ResponseBodyLoggingMiddleware>();
+
+                #endregion
+
+                #region Middlewares
+                var app = builder.Build();
+
+                app.UseCors("default");
+
+                //Global exception handaling 
+                app.UseExceptionHandler(errorApp =>
+                {
+                    errorApp.Run(async context =>
+                    {
+                        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                        var exception = exceptionHandlerPathFeature?.Error;
+
+                        Log.Error(exception, "Unhandled exception occurred. {ExceptionDetails}", exception?.ToString());
+                        Console.WriteLine(exception?.ToString());
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        await context.Response.WriteAsync("An unexpected error occurred. Please try again later.");
+                    });
+                });
+
+                //Middle ware for seri log & application insights
+                app.UseMiddleware<RequestResponseLoggingMiddleware>();
+                app.UseMiddleware<RequestBodyLoggingMiddleware>();
+                app.UseMiddleware<ResponseBodyLoggingMiddleware>();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+
+                app.MapControllers();
+
+                app.Run();
+                #endregion
             }
-
-            app.UseCors("default"); 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-            #endregion
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+           
         }
     }
 }
