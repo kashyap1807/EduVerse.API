283bf0dbaf32240f2440b25659ee29e31112e039
Kashyap <kashyapchudasama0@gmail.com>
2025-04-23 20:54:35 +0530

    UserAdmin Controller created

---
+++ EduVerse.API/Controllers/UserAdminController.cs
@@ -0,0 +1,26 @@
+﻿using EduVerse.Core.Dtos;
+using EduVerse.Service.Contract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduVerse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAdminController : ControllerBase
+    {
+        private readonly ICourseService courseService;
+
+        public UserAdminController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserDto>>> GetAllUsersAsync()
+        {
+            var courses = await courseService.GetAllCoursesAsync();
+            return Ok(courses);
+        }
+    }
+}
