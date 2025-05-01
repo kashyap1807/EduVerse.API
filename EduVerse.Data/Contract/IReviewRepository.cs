faef7b6ee9a0b5b2a605e13e48360ff496e32d65
kashyap1807 <kashyapchudasama0@gmail.com>
2025-05-01 22:05:51 +0530

    Review Repository Completed

---
+++ EduVerse.Data/Contract/IReviewRepository.cs
@@ -0,0 +1,19 @@
+﻿using EduVerse.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduVerse.Data.Contract
+{
+    public interface IReviewRepository
+    {
+        Task<Review?> GetReviewByIdAsync(int id);
+        Task<IEnumerable<Review>> GetReviewByCourseIdAsync(int courseId);
+        Task<IEnumerable<Review>> GetReviewByUserIdAsync(int userId);
+        Task AddReviewAsync(Review review);
+        Task UpdateReviewAsync(Review review);
+        Task DeleteReviewAsync(int reviewId);
+    }
+}
