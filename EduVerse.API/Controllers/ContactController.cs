822004d9d09cbc9d8449629a200bb14bf0cc10ed
kashyap1807 <kashyapchudasama0@gmail.com>
2025-05-14 19:45:18 +0530

    Contact controller created

---
+++ EduVerse.API/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using EduVerse.Core.Dtos;
+using EduVerse.Service.Contract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduVerse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IEmailNotification emailNotification;
+
+        public ContactController(IEmailNotification emailNotification)
+        {
+            this.emailNotification = emailNotification;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage([FromBody] ContactMessageDto contactMessageDto)
+        {
+            if(contactMessageDto == null)
+            {
+                return BadRequest("Contact message cannot be null");
+            }
+
+            await emailNotification.SendEmailForContactUs(contactMessageDto);
+
+            return Ok(new { message = "Message sent successfully!", model = contactMessageDto });
+        }
+
+    }
+}
