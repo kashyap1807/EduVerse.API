7db87783f3d80d136f0071d8e3e48fb517e71713
kashyap1807 <kashyapchudasama0@gmail.com>
2025-06-01 21:42:40 +0530

    Add Review Update

--- EduVerse.Core/Dtos/UserReviewDto.cs
+++ EduVerse.Core/Dtos/UserReviewDto.cs
@@ -12,7 +12,7 @@ namespace EduVerse.Core.Dtos
         public int ReviewId { get; set; }
         public int CourseId { get; set; }
         public int UserId { get; set; }
-        public string UserName { get; set; } = string.Empty;
+        public string? UserName { get; set; } = string.Empty;
         public int Rating { get; set; }
         public string? Comments { get; set; }
         public DateTime ReviewDate { get; set; }
